db6ca6f3bd277fe215ad39490f436a8948cf4a25
fjmartinez <fjmartinezduran@gmail.com>
2021-07-29 17:57:25 +0200

    medidas preventivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,55 +16,58 @@ export class AppComponent {
 
   public obs$!: Observable<any>;
 
-  public localidadSeleccionada:Localidad={nombre: 'Peñaflor', dataValue:3101};
+  public localidadSeleccionada:Localidad={nombre: 'Peñaflor', dataValue:3101, detailMapaCovid:'Peñaflor'};
 
   public provincias:Provincia[]=[
     {
       nombre:'Cádiz',
       localidades:[
-        {nombre: 'Bornos', dataValue:2363},
-        {nombre: 'Conil de la Frontera', dataValue:2367},
-        {nombre: 'Jerez de la Frontera', dataValue:2373},
+        {nombre: 'Bornos', dataValue:2363, detailMapaCovid:'Bornos'},
+        {nombre: 'Conil de la Frontera', dataValue:2367, detailMapaCovid:'Conil%20de%20la%20Frontera'},
+        {nombre: 'Jerez de la Frontera', dataValue:2373, detailMapaCovid:'Jerez%20de%20la%20Frontera'},
 
       ]
     },
     {
         nombre:'Córdoba',
         localidades:[
-          {nombre: 'Castro del Río', dataValue:2421},
-          {nombre: 'Córdoba (capital)', dataValue:2423},
-          {nombre: 'Hornachuelos', dataValue:2438},
-          {nombre: 'Palma del Río', dataValue:2451},
-          {nombre: 'Posadas', dataValue:2455},
+          {nombre: 'Castro del Río', dataValue:2421, detailMapaCovid: 'Castro%20del%20Río'},
+          {nombre: 'Córdoba (capital)', dataValue:2423, detailMapaCovid:'Córdoba'},
+          {nombre: 'Hornachuelos', dataValue:2438, detailMapaCovid:'Hornachuelos'},
+          {nombre: 'Palma del Río', dataValue:2451, detailMapaCovid:'Palma%20del%20Río'},
+          {nombre: 'Posadas', dataValue:2455, detailMapaCovid:'Posadas'},
         ]
       },
       {
         nombre:'Huelva',
         localidades:[
-          {nombre: 'Isla Cristina', dataValue:2755},
-          {nombre: 'Punta Umbría', dataValue:2773},
+          {nombre: 'Almonte', dataValue:2718, detailMapaCovid:'Almonte'},
+          {nombre: 'Isla Cristina', dataValue:2755, detailMapaCovid:'Isla%20Cristina'},
+          {nombre: 'Punta Umbría', dataValue:2773, detailMapaCovid:'Punta%20Umbría'},
         ]
       },
       {
         nombre:'Sevilla',
         localidades:[
-          {nombre: 'Cazalla de la Sierra', dataValue:3057},
-          {nombre: 'Constantina', dataValue:3058},
-          {nombre: 'Dos Hermanas', dataValue:3063},
-          {nombre: 'Écija', dataValue:503755},
-          {nombre: 'El Pedroso', dataValue:3100},
-          {nombre: 'La Campana', dataValue:3047},
-          {nombre: 'La Puebla de los Infantes', dataValue:3105},
-          {nombre: 'Lora del Río', dataValue:3082},
-          {nombre: 'Mairena del Alcor', dataValue:3085},
-          {nombre: 'Peñaflor', dataValue:3101},
-          {nombre: 'Sevilla (capital)', dataValue:3120},
-          {nombre: 'Tocina', dataValue:3121},
+          {nombre: 'Cazalla de la Sierra', dataValue:3057, detailMapaCovid:'Cazalla%20de%20la%20Sierra'},
+          {nombre: 'Constantina', dataValue:3058, detailMapaCovid:'Constantina'},
+          {nombre: 'Dos Hermanas', dataValue:3063, detailMapaCovid:'Dos%20Hermanas'},
+          {nombre: 'Écija', dataValue:503755, detailMapaCovid:'Écija'},
+          {nombre: 'El Pedroso', dataValue:3100, detailMapaCovid:'El%20Pedroso'},
+          {nombre: 'La Campana', dataValue:3047, detailMapaCovid:'La%20Campana'},
+          {nombre: 'La Puebla de los Infantes', dataValue:3105, detailMapaCovid:'La%20Puebla%20de%20los%20Infantes'},
+          {nombre: 'Lora del Río', dataValue:3082, detailMapaCovid:'Lora%20del%20Río'},
+          {nombre: 'Mairena del Alcor', dataValue:3085, detailMapaCovid:'Mairena%20del%20Alcor'},
+          {nombre: 'Peñaflor', dataValue:3101, detailMapaCovid:'Peñaflor'},
+          {nombre: 'Sevilla (capital)', dataValue:3120, detailMapaCovid:'Sevilla'},
+          {nombre: 'Tocina', dataValue:3121, detailMapaCovid:'Tocina'},
         ]
       }
     ];  
 
     public expande:boolean=false;
+
+    public mapaCovid:string="https://www.mapacovid.es/detail/Peñaflor";
 
   constructor(private api:ServiceService, private iconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer,){
@@ -92,6 +95,7 @@ export class AppComponent {
     this.localidadSeleccionada=localidad;    
     localStorage.setItem('localidad',JSON.stringify(this.localidadSeleccionada));
     this.obs$=this.api.loadCovidData(localidad.dataValue);
+    this.mapaCovid=`https://www.mapacovid.es/detail/${this.localidadSeleccionada.detailMapaCovid}`;
   }
 
   public async openSweetInputPhoneNumber() {
@@ -122,10 +126,11 @@ export class AppComponent {
 
 interface Localidad {
   nombre: string;
-  dataValue:number
+  dataValue:number;
+  detailMapaCovid:string;
 }
 
 interface Provincia{
   nombre:string;
-  localidades: Localidad[]
+  localidades: Localidad[];
 }
